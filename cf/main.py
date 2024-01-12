ef4e6688a5d76a76ab1e9c45a76b3487e851f036
szche <szymon@chadam.pl>
2024-01-12 18:50:42 +0100

    html generation

--- cf/main.py
+++ cf/main.py
@@ -1,4 +1,6 @@
 import sqlalchemy
+from google.cloud import storage
+import datetime
 
 
 connection_name = "testsabregcp:us-central1:chadam-db-sql-mysql"
@@ -12,14 +14,19 @@ query_string = dict({"unix_socket": "/cloudsql/{}".format(connection_name)})
 
 creator = sqlalchemy.text("CREATE TABLE IF NOT EXISTS info (id INT NOT NULL AUTO_INCREMENT, date DATE, hours INT, PRIMARY KEY (id));")
 
+def upload_blob(bucket_name, blob_text, destination_blob_name):
+    """Uploads a file to the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+    html = f"<html><h1>Invoice</h1>Service: IT Services </br>Quantity: {int(blob_text)} hours </br>Rate: 10 PLN / hour </br>Total: {int(blob_text) * 10} PLN</br></html>"
+    blob.upload_from_string(html, content_type="text/html")
+
+
 
 def writeToSql(request):
    request_json = request.get_json(silent=True)
-   date = request_json["date"]
-   hours = request_json["hours"]
-   print(date, hours)
-
-   stmt = sqlalchemy.text(f"INSERT INTO info ( date, hours ) values ('{date}', {hours})")
+   action = request_json["action"]
 
    db = sqlalchemy.create_engine(
    sqlalchemy.engine.url.URL(
@@ -37,11 +44,23 @@ def writeToSql(request):
    try:
       with db.connect() as conn:
          conn.execute(creator)
-         conn.execute(stmt)
-         print("Insert successful")
+         if action == "save":
+             date = request_json["date"]
+             hours = request_json["hours"]
+             stmt = sqlalchemy.text(f"INSERT INTO info ( date, hours ) values ('{date}', {hours})")
+             conn.execute(stmt)
+             print("Insert successful")
+         elif action == "invoice":
+             month = request_json["month"]
+             stmt = sqlalchemy.text(f"SELECT SUM(hours) FROM info WHERE MONTH(date) = {month}")
+             data = str(conn.execute(stmt).fetchall()[0][0])
+             print(data)
+             upload_blob("chadam-sabre-gcp-bucket", data, f"invoice-{month}.html")
+             print(data)
    except Exception as e:
       print ("Some exception occured" + str(e))
       return 'Error: {}'.format(str(e))
    return 'ok'
+
 
 
